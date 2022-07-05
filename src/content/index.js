eb456be6e0e2839c253af25ea701229f874acb39
Faruk Arıgün <mail@farukarigun.com>
2022-07-05 12:54:52 +0300

    Change Logo

--- src/content/index.js
+++ src/content/index.js
@@ -7,7 +7,7 @@ if (window.location.host === 'www.npmjs.com') {
     '#top > div.fdbf4038.w-third-l.mt3.w-100.ph3.ph4-m.pv3.pv0-l > div:nth-child(3) > p > a',
   ).href
 
-  console.log(nodes)
+  // console.log(nodes)
 
   window.location.replace(nodes);
 
